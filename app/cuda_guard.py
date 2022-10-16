61f855f3e625c96bfdef95ef84f408c3998565f8
Katrin Schmid <kabischmid@gmail.com>
2022-10-16 16:11:11 +1100

    import

--- app/cuda_guard.py
+++ app/cuda_guard.py
@@ -6,6 +6,7 @@
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION & AFFILIATES is strictly prohibited.
 import sys 
+import pycuda.autoinit, pycuda.gl as cuda_gl
 
 def setup_cuda_context():
     """ Carefully load CUDA based frameworks to avoid interference.
